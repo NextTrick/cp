534e79ebdaca80fbed8ca0b87e603c706aad501b
Luis Felipe <lfna23@gmail.com>
2016-04-06 15:27:23 -0500

    feat(config): sprites

--- public/s/front/gulpfile.js
+++ public/s/front/gulpfile.js
@@ -4,7 +4,8 @@ var gulp = require('gulp'),
     browserSync = require('browser-sync'),
     coffee = require('gulp-coffee'),
     stylus = require('gulp-stylus'),
-    copy = require('gulp-imagemin');
+    copy = require('gulp-imagemin'),
+    sprites = require('gulp.spritesmith');
 
 // Static server
 gulp.task('server', function () {
@@ -30,7 +31,9 @@ gulp.task('coffee', function(){
 
 gulp.task('stylus', function () {
     return gulp.src('./stylus/*.styl')
-    .pipe(stylus())
+    .pipe(stylus({
+        'include css':true
+    }))
     .pipe(gulp.dest('./dist/css'))
 });
 
@@ -38,6 +41,28 @@ gulp.task('copy:img', function() {
     return gulp.src('./images/*.*')
     .pipe(copy())
     .pipe(gulp.dest('./dist/images'));
+});
+
+gulp.task('sprites', function () {
+    var spriteData = gulp.src('./images/sprites/*.png')
+      .pipe(sprites({
+        algorithm: 'binary-tree',
+        imgName: 'sprite.png',
+        cssName: 'sprite.styl',
+        cssTemplate : './images/sprite_template/stylus.template.handlebars'
+    }));
+    //return spriteData.pipe(gulp.dest('./images/'));
+    // Pipe image stream through image optimizer and onto disk 
+    var imgStream = spriteData.img
+    // DEV: We must buffer our stream into a Buffer for `imagemin` 
+        .pipe(gulp.dest('./images/'));
+     
+    // Pipe CSS stream through CSS optimizer and onto disk 
+    var cssStream = spriteData.css
+        .pipe(gulp.dest('./stylus/sprite/'));
+     
+    // Return a merged stream to handle both `end` events 
+    return spriteData;
 });
 // Default task
 //gulp.task('default', ['styles', 'scripts', 'images', 'markdown', 'jade', 'watch']);
