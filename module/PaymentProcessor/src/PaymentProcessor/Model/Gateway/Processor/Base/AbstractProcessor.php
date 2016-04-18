b7e8938aafa4949a193e1f9c7905c8ff7a22f580
NextTrick <ing.angeljara@gmail.com>
2016-04-18 17:02:14 -0500

    udpate

--- module/PaymentProcessor/src/PaymentProcessor/Model/Gateway/Processor/Base/AbstractProcessor.php
+++ module/PaymentProcessor/src/PaymentProcessor/Model/Gateway/Processor/Base/AbstractProcessor.php
@@ -18,5 +18,20 @@ abstract class AbstractProcessor
         return $this->sl;
     }
     
+    public function getViewXml($path, $data = array())
+    {
+        $resolver = new TemplatePathStack(array(
+            'script_paths' => array(__DIR__ . '/../Ws/')
+        ));
+        
+        $renderer = new PhpRenderer();
+        $renderer->setResolver($resolver);
+        $view = new ViewModel();
+        $view->setTemplate($path);
+        $view->setVariables($data);
+        
+        return $renderer->render($view);
+    }
+    
     abstract public function createCharge($data);    
 }
