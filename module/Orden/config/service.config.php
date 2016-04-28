a15f1222b3e1c848fdcc095a0a7df2e68e2ea732
diomedes pablo <diomedex10@gmail.com>
2016-04-27 20:03:42 -0500

    Reporte Pagos

--- module/Orden/config/service.config.php
+++ module/Orden/config/service.config.php
@@ -9,6 +9,7 @@ return array(
         //forms
         'Orden\Form\CarritoForm' => 'Orden\Form\Factory\CarritoFactory',
         'Orden\Form\OrdenForm' => 'Orden\Form\Factory\OrdenFactory',
+        'Orden\Form\BuscarForm'  => 'Orden\Form\Factory\BuscarFactory',
     ),
     'invokables' => array(
     ),
--- module/Orden/src/Orden/Model/Service/OrdenService.php
+++ module/Orden/src/Orden/Model/Service/OrdenService.php
@@ -13,6 +13,13 @@ class OrdenService
     protected $_repository = null;
     protected $_sl = null;
 
+    CONST TIPO_COMPROBANTE_DNI = 1;
+    CONST TIPO_COMPROBANTE_RUC = 2;
+
+    CONST METODO_PAGO_VISA   = 1;
+    CONST METODO_PAGO_PE     = 2;
+    CONST METODO_PAGO_MASTER = 3;
+
     public function __construct($repository, $serviceLocator)
     {
         $this->_repository = $repository;
