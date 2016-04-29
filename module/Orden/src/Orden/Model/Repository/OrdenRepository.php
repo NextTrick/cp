f60b443c008cc79638ac3b45e7c0ca7d8bde6054
diomedes pablo <diomedex10@gmail.com>
2016-04-28 21:42:52 -0500

    seccion pagos

--- module/Orden/src/Orden/Model/Repository/OrdenRepository.php
+++ module/Orden/src/Orden/Model/Repository/OrdenRepository.php
@@ -51,8 +51,10 @@ class OrdenRepository extends \Common\Model\Repository\Zf2AbstractTableGateway
             }
 
             foreach ($this->crWhereBetween as $key => $value) {
-                if (!empty($value['min']) && !empty($value['max']) && !empty($key)) {
-                    $where->and->between($key, $value['min'], $value['max']) ;
+                if (!empty($value['min']) && !empty($key)) {
+                    $where->and->lessThanOrEqualTo($key, $value['min']) ;
+                } elseif (!empty($value['max']) && !empty($key)) {
+                    $where->and->greaterThanOrEqualTo($key, $value['max']) ;
                 }
             }
 
