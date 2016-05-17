d840871df46af73746a54620f679a61c217bdf83
NextTrick <ing.angeljara@gmail.com>
2016-05-17 04:13:07 -0500

    update

--- module/PaymentProcessor/src/PaymentProcessor/Model/Gateway/Processor/Ws/Visa.php
+++ module/PaymentProcessor/src/PaymentProcessor/Model/Gateway/Processor/Ws/Visa.php
@@ -176,7 +176,7 @@ class Visa
         $createEticketRequestData = array(
             'commerceCode' => $this->config['codigoComercio'],
             'id' => $data['id'],
-            'amount' => $data['monto'],
+            'amount' => (string) $data['monto'],
             'profileName' => $data['perfilpago_nombres'],
             'profileLastName' => $data['perfilpago_paterno'] . ' ' . $data['perfilpago_materno'],
             'city' => 'LIMA',
