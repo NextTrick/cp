3d5f9c1c2c31b69ac97b9780c6508698a092087a
NextTrick <ing.angeljara@gmail.com>
2016-05-19 12:15:07 -0500

    update

--- module/Orden/src/Orden/Model/Repository/DetalleOrdenRepository.php
+++ module/Orden/src/Orden/Model/Repository/DetalleOrdenRepository.php
@@ -88,7 +88,7 @@ class DetalleOrdenRepository extends \Common\Model\Repository\Zf2AbstractTableGa
         $select->join(array('b' => 'paquete_paquete'), 'a.paquete_id = b.id',
             array('paquete_id' => 'id', 'paquete_titulo1' => 'titulo1', 'paquete_titulo2' => 'titulo2'));
         $select->join(array('c' => 'tarjeta_tarjeta'), 'a.tarjeta_id = c.id',
-            array('tarjeta_id' => 'id', 'tarjeta_nombre' => 'nombre'));
+            array('tarjeta_id' => 'id', 'tarjeta_nombre' => 'nombre', 'tarjeta_numero' => 'numero'));
 
         $select->where->equalTo('a.id', $ordenId);
 
