0f0b8208d3d1aa617c4e866ae2a11fe94c960078
juan.ludena <juan.ludena@bongous.com>
2016-05-18 20:18:51 -0500

    fix user admin

--- module/Admin/src/Admin/Controller/LoginController.php
+++ module/Admin/src/Admin/Controller/LoginController.php
@@ -64,18 +64,116 @@ class LoginController extends SecurityAdminController
 
     public function recuperarPasswordAction()
     {
+        $form = $this->_getRecuperarPasswordForm();
+        $form->setAttribute('action', $this->url()->fromRoute('admin/crud', array(
+            'controller' => 'login',
+            'action' => 'recuperar-password',
+        )));
         
-    }
-    
-    public function modificarPasswordAction()
-    {
+        if ($this->request->isPost()) {
+            $form->setInputFilter(new \Application\Filter\RecuperarPasswordFilter());
+            $data = $this->request->getPost()->toArray();
+            $form->setData($data);
+            
+            $criteria = array('where' => array('email' => $data['email']));
+            $usuario = $this->_getUsuarioService()->getRepository()->findOne($criteria);
+
+            $noExistsEmail = 'El correo electrónico no se ecuentra registrado.';
+            if (empty($usuario)) {
+                $form->get('email')->setMessages(array('noExistsEmail' => $noExistsEmail));
+            } elseif ($form->isValid()) {
+                //generar y guardar token de verificación
+                $codigoRecuperar = \Common\Helpers\Util::generateToken($usuario['id']);
+                $this->_getUsuarioService()->getRepository()->save(array(
+                    'codigo_activar' => $codigoRecuperar
+                ), $usuario['id']);
+
+                $serviceLocator = $this->getServiceLocator();
+                $email = new \Usuario\Model\Email\RecuperarPassword($serviceLocator);
+                $data['codigo_activar'] = $codigoRecuperar;
+                $ok = $email->sendMail($data);
+                if ($ok) {
+                    $message = 'Te hemos enviado un correo con las instrucciones para actualizar tu cuenta';
+                    $this->flashMessenger()->addMessage(array(
+                        'success' => $message,
+                    ));
+                    return $this->redirect()->toRoute('admin/recuperar-password');
+                } else {
+                    $errorSend = 'Error al eviar correo electrónico.';
+                    $form->get('email')->setMessages(array('errorSend' => $errorSend));
+                }
+            }
+        }
         
+        $this->layout('layout/login');
+        $view = new ViewModel();
+        $view->setVariable('form', $form);
+        return $view;
     }
 
+    public function modificarPasswordAction()
+    {
+        $codigo = $this->params('codigo', null);
+        $form = $this->_getModificarPasswordForm();
+        $form->setAttribute('action', $this->url()->fromRoute('admin/modificar-password', array(
+            'controller' => 'login',
+            'action' => 'modificar-password',
+            'codigo' => $codigo,
+        )));
+        $form->get('codigo')->setValue($codigo);
+        
+        if ($this->request->isPost()) {
+            $form->setInputFilter(new \Application\Filter\ModificarPasswordFilter());
+            $data = $this->request->getPost()->toArray();
+            $form->setData($data);
+            if ($form->isValid()) {
+                $data = $form->getData();
+                $repository = $this->_getUsuarioService()->getRepository();
+                $criteria = array('where' => array('codigo_activar' => $data['codigo']));
+                $row = $repository->findOne($criteria);
+                if (!empty($row)) {
+                    $dataIn = array(
+                        'password' => \Common\Helpers\Util::passwordHash($data['password']),
+                        'codigo_activar' => null,
+                    );
+                    $repository->save($dataIn, $row['id']);
+                    $this->flashMessenger()->addMessage(array(
+                        'success' => 'Su password fué actualizada correctamente.',
+                    ));
+                    return $this->redirect()->toRoute('admin/login');
+                } else {
+                    $this->flashMessenger()->addMessage(array(
+                        'error' => 'Error al actualizar su password.',
+                    ));
+                }
+            }
+        }
+        
+        $this->layout('layout/login');
+        $view = new ViewModel();
+        $view->setVariable('form', $form);
+        return $view;
+    }
+    
     public function logoutAction()
     {
         $this->_getLoginService()->getRepository()->logout();
         return $this->_toUrlLogin();
+    }
+    
+    private function _getRecuperarPasswordForm()
+    {
+        return $this->getServiceLocator()->get('Admin\Form\RecuperarPasswordForm');
+    }
+    
+    private function _getModificarPasswordForm()
+    {
+        return $this->getServiceLocator()->get('Admin\Form\ModificarPasswordForm');
+    }
+    
+    protected function _getUsuarioService()
+    {
+        return $this->getServiceLocator()->get('Admin\Model\Service\UsuarioService');
     }
 
     protected function _getLoginForm()
---
+++ module/Admin/src/Admin/Filter/ModificarPasswordFilter.php
@@ -0,0 +1,66 @@
+<?php
+/*
+ * Autor       : Juan Carlos Ludeña Montesinos
+ * Año         : Mayo 2016
+ * Descripción :
+ *
+ */
+
+namespace Admin\Filter;
+use Common\Filter\Zf2InputFilter;
+
+class ModificarPasswordFilter extends Zf2InputFilter
+{
+    protected $_values = array();
+
+    public function __construct($values = array())
+    {
+        $this->_values = $values;
+        
+        $this->_addElements();
+    }
+
+    protected function _addElements()
+    {
+        $this->add(array(
+            'name' => 'password',
+            'required' => true,
+            'filters'  => array(
+                array('name' => 'StripTags'),
+                array('name' => 'StringTrim'),
+            ),
+            'validators' => array(
+                self::validatorNotEmpty('Contraseña'),
+                self::validatorStringLength('contraseña'),
+            )
+        ));
+
+        $this->add(array(
+            'name' => 'password_repeat',
+            'required' => true,
+            'filters'  => array(
+                array('name' => 'StripTags'),
+                array('name' => 'StringTrim'),
+            ),
+            'validators' => array(
+                self::validatorNotEmpty('Repetir contraseña'),
+                array(
+                    'name' => 'Identical',
+                    'options' => array(
+                        'token' => 'password',
+                        'message' => array(
+                            \Zend\Validator\Identical::NOT_SAME => "Las contraseñas ingresadas no coinciden.",
+                            \Zend\Validator\Identical::MISSING_TOKEN => 'Repetir contraseña por favor.',
+                        ),
+                        'break_chain_on_failure' => true,
+                    ),
+                ),
+            )
+        ));
+    }
+
+    protected function _getValueDefault($key)
+    {
+        return isset($this->_values[$key]) ? $this->_values[$key] : null;
+    }
+}
