940d4901c850b96f97561e85c1351f40f796f365
NextTrick <ing.angeljara@gmail.com>
2016-05-22 02:58:21 -0500

    update

--- module/Orden/src/Orden/Model/Repository/DetalleOrdenRepository.php
+++ module/Orden/src/Orden/Model/Repository/DetalleOrdenRepository.php
@@ -93,6 +93,7 @@ class DetalleOrdenRepository extends \Common\Model\Repository\Zf2AbstractTableGa
         $select->where->equalTo('a.id', $ordenId);
 
         $statement = $sql->prepareStatementForSqlObject($select);
+        echo $statement->getSql(); exit;
         $data = $this->resultSetPrototype->initialize($statement->execute())->toArray();
 
         return $data;
