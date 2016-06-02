b59b1e51e34bf40cdacf925041f631ed2c87660a
juan.ludena <juan.ludena@bongous.com>
2016-06-02 01:01:28 -0500

    fix login user

--- web/Application/src/Application/Controller/RecargasController.php
+++ web/Application/src/Application/Controller/RecargasController.php
@@ -14,15 +14,25 @@ class RecargasController extends SecurityWebController
 
         $tarjetaCodigo = $this->params('codigo');
         $usuario = $this->_getUsuarioData();
+        $usuarioTarjetas = $this->_getTarjetaService()->getDdlTarjetas($usuario->id);
+        
+        if (empty($usuarioTarjetas)) {
+            return $this->redirect()->toRoute('web-mis-tarjetas', array('controller' => 'index'));
+        }
+        
+        if (empty($tarjetaCodigo)) {
+            foreach ($usuarioTarjetas as $key => $value) {
+                $tarjetaCodigo = $key;
+                break;
+            }
+        }
+        
         $cartModel = $this->_getCartService()->getCart();
         if (!empty($cartModel)) {
             $cartModel->setGroupProduct($tarjetaCodigo);
         }
         
-        $usuarioTarjetas = $this->_getTarjetaService()->getDdlTarjetas($usuario->id);
-        if (empty($usuarioTarjetas)) {
-            return $this->redirect()->toRoute('web-mis-tarjetas', array('controller' => 'index'));
-        }
+        
         
         $view = new ViewModel();
         $view->setVariable('cartModel', $cartModel);
