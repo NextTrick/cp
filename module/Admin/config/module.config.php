cb261ad879d95c5b99798efd7c8219ea8c148398
NextTrick <ing.angeljara@gmail.com>
2016-06-02 10:39:32 -0500

    update

--- module/Admin/config/module.config.php
+++ module/Admin/config/module.config.php
@@ -44,12 +44,12 @@ return array(
         'display_not_found_reason' => true,
         'display_exceptions'       => true,
         'doctype'                  => 'HTML5',
-        'not_found_template'       => 'error/404',
-        'exception_template'       => 'error/index',
+        //'not_found_template'       => 'error/404',
+        //'exception_template'       => 'error/index',
         'template_map' => array(
             'layout/admin-layout'           => __DIR__ . '/../view/layout/admin-layout.phtml',
-            'error/404'               => __DIR__ . '/../view/error/404.phtml',
-            'error/index'             => __DIR__ . '/../view/error/index.phtml',
+//            'error/404'               => __DIR__ . '/../view/error/404.phtml',
+//            'error/index'             => __DIR__ . '/../view/error/index.phtml',
         ),
         'template_path_stack' => array(
             __DIR__ . '/../view',
