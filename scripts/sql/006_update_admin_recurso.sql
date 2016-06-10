ffe97c545b8dd304bec521c4dd4d435bb1c420cc
jludena <montesinos2005ii@gmail.com>
2016-06-10 01:00:30 -0500

    alter table

--- scripts/sql/006_update_admin_recurso.sql
+++ scripts/sql/006_update_admin_recurso.sql
@@ -1,1 +1,9 @@
+UPDATE admin_recurso SET `url` = 'admin/usuarioadmin' WHERE id = 4;
 UPDATE admin_recurso SET `url` = 'admin/recurso' WHERE id = 5;
+UPDATE admin_recurso SET `url` = 'admin/rol' WHERE id = 6;
+UPDATE admin_recurso SET `url` = 'admin/permiso' WHERE id = 7;
+UPDATE admin_recurso SET `url` = 'admin/paquete' WHERE id = 9;
+UPDATE admin_recurso SET `url` = 'admin/usuario' WHERE id = 11;
+UPDATE admin_recurso SET `url` = 'admin/orden' WHERE id = 12;
+UPDATE admin_recurso SET `url` = 'admin/ordendetalle' WHERE id = 13;
+UPDATE admin_recurso SET `url` = 'admin/contenido' WHERE id = 15;
