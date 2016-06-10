9da7bd2640ba172dc8d77d7add97b8b80a15d9c1
jludena <montesinos2005ii@gmail.com>
2016-06-09 22:50:27 -0500

    alter table

--- config/autoload/constants.php
+++ config/autoload/constants.php
@@ -1,8 +1,7 @@
 <?php
-define('BASE_URL', 'http://dev.recargas.coneypark.pe/');
-define('URL_RESOURCES', 'http://dev.recargas.coneypark.pe/s/');
-//define('BASE_URL', 'http://dev.coneypark.pe/recargas/');
-//define('URL_RESOURCES', 'http://dev.coneypark.pe/recargas/s/');
+
+define('BASE_URL', 'http://dev.coneypark.pe/recargas/');
+define('URL_RESOURCES', 'http://dev.coneypark.pe/recargas/s/');
 
 define('URL_IMAGE_CMS', rtrim(URL_RESOURCES, '/').'/files/contenido/');
 
