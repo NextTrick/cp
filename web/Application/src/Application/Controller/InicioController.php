2d22657ed3ac13c1f14cb57c3d4d1331e05258a3
jludena <montesinos2005ii@gmail.com>
2016-06-09 23:41:21 -0500

    alter table

--- web/Application/src/Application/Controller/InicioController.php
+++ web/Application/src/Application/Controller/InicioController.php
@@ -8,6 +8,8 @@ class InicioController extends AbstractActionController
 {
     public function indexAction()
     {
+        session_start();
+        var_dump($_SESSION);exit;
         echo 'Pantalla Inicio:';
         exit;
         $view = new ViewModel();
