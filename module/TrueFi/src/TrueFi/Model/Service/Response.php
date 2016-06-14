a1b7a770d3b85972a8ef64bb1794fcf08fa83905
NextTrick <ing.angeljara@gmail.com>
2016-06-14 14:23:54 -0500

    update

--- module/TrueFi/src/TrueFi/Model/Service/Response.php
+++ module/TrueFi/src/TrueFi/Model/Service/Response.php
@@ -18,7 +18,7 @@ class Response
             'curloptions' => array(
                 CURLOPT_SSL_VERIFYHOST => false,
                 CURLOPT_SSL_VERIFYPEER => false,
-                CURLOPT_TIMEOUT => 300,
+                CURLOPT_TIMEOUT => 240,
             ),
         ));
 
